57f31a8a9cc9f6895f59f3c519663ea7067de95b
Dr Xam <samuelhappiness360@gmail.com>
2025-01-19 16:49:01 +0100

    Drag and Drop functionality

--- Sortable List/script.js
+++ Sortable List/script.js
@@ -45,5 +45,54 @@ function createList() {
             draggable.appendChild(listItem);
         });
 
-    // addEventListeners();
+    addEventListeners();
+}
+
+function dragStart() {
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}   
+
+function dragDrop() {
+    const dragEndIndex = +this.getAttribute('data-index');
+
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}   
+
+function dragEnter() {
+    this.classList.add('over');
+}
+
+function dragLeave() {
+    this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    });
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    });
 }
